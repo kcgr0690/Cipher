42e763016ec6f93183df5d5b8e91e49e3f4e9405
kcgr0690 <kcgr0690@gmail.com>
2025-10-05 16:13:26 -0400

    Optimized to flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from app import Flask, render_template, request
+from cipher import encode, decode
+
+app = Flask(name)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    result = ""
+    mode = ""
+    input_text = ""
+    if request.method == 'POST':
+        mode = request.form.get('mode')
+        input_text = request.form.get('input_text', '')
+        if mode == 'encode':
+            result = encode(input_text)
+        elif mode == 'decode':
+            result = decode(input_text)
+    return render_template('index.html', result=result, mode=mode, input_text=input_text)
+
+if name == 'main':
+    app.run()
